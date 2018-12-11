579878b89d409c1c6b71088848b1e41ab53c9407
hobbes9469 <mattkang202@gmail.com>
2018-12-11 14:37:32 -0800

    Update ACO.lisp

--- ACO.lisp
+++ ACO.lisp
@@ -91,6 +91,15 @@
 				(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0)))
 
 
+; List of ants to be deployed on the grid
+(defvar antlist '(a1  a2  a3  a4  a5  a6  a7  a8  a9  a10
+                  a11 a12 a13 a14 a15 a16 a17 a18 a19 a20
+                  a21 a22 a23 a24 a25 a26 a27 a28 a29 a30
+                  a31 a32 a33 a34 a35 a36 a37 a38 a39 a40
+                  a41 a42 a43 a44 a45 a46 a47 a48 a49 a50))
+
+; Counter to keep track of how many ants are on the grid
+(defvar antcounter 0)
 
 
 ; Ant class definition
@@ -233,7 +242,5 @@
 
 
 
-; TODO: Implement avoiding the last T = 8 Tabu cells for the ant
-;       Otherwise, the ant will get stuck around (4 8)
-;       Probably should be done in the "bestsquare" function
-
+; TODO: Start to structure the iteration skeleton
+;       starting with antstep, 
